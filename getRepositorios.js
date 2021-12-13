d7ceee187c6268835ad3780a98c78e8ef389dbe7
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-12-13 09:52:06 -0300

    Inclusão do parâmetro search_namespaces=true na URL de busca de repositórios

--- getRepositorios.js
+++ getRepositorios.js
@@ -1,7 +1,7 @@
 const getDadosAPI = require("./getDadosAPI");
 
 module.exports = async function getRepositorios(gitlabUrl, token, filtroGrupo) {
-  const endpointURL = `https://${gitlabUrl}/api/v4/projects?search=${filtroGrupo}`;
+  const endpointURL = `https://${gitlabUrl}/api/v4/projects?search_namespaces=true&search=${filtroGrupo}`;
   const repos = await getDadosAPI(endpointURL, token);
   //filtra só por aqueles cujo path inicial por 'filtroGrupo' e ordena pelo path completo do repositório
   return repos
